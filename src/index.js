0ca0ec682eb86dcd2e97bbbd4cb471dfd84581a6
anshul-tatware1712 <anshultatware01@gmail.com>
2025-12-07 14:51:35 +0530

    added schema

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 app.get("/start", (req, res) => {
   res.send("livloc360 backend running ✅");
